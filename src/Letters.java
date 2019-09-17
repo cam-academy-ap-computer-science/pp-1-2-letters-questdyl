69bc2f3285ac0db5b9d4691f75e6f9438fb7208b
sillybop <sillybop@10.0.0.164>
2019-09-16 20:34:35 -0700

    Done with 1.5. (I mixed up 5 and 2 by accident)

--- src/Letters.java
+++ src/Letters.java
@@ -2,8 +2,39 @@
 public class Letters {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// The House That Jack Built
+		System.out.println("This is the house that Jack built.");
+		System.out.println("\nThis is the malt");
+		System.out.println("That lay in the house that Jack built.");
+		System.out.println("\nThis is the rat,");
+		malt();
+		System.out.println("\nThis is the cat,");
+		rat();
+		System.out.println("\nThis is the dog,");
+		cat();
+		System.out.println("\nThis is the cow with the crumpled horn,");
+		dog();
+		System.out.println("\nThis is the maiden all forlorn");
+		cow();
 	}
-
+	public static void malt() {
+		System.out.println("That ate the malt");
+		System.out.println("That lay in the house that Jack built.");
+	}
+	public static void rat() {
+		System.out.println("That killed the rat,");
+		malt();
+	}
+	public static void cat() {
+		System.out.println("That worried the cat,");
+		rat();
+	}
+	public static void dog() {
+		System.out.println("That tossed the dog,");
+		cat();
+	}
+	public static void cow() {
+		System.out.println("That milked the cow with the crumpled horn,");
+		dog();
+	}
 }
